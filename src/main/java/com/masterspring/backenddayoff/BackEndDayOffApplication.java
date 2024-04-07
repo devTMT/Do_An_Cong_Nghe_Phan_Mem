2350c7eed26eed29864d83cdc74e24e58ef2957e
ntandat <20130221@st.hcmuaf.edu.vn>
2024-04-07 09:18:25 +0700

    First commit

---
+++ src/main/java/com/masterspring/backenddayoff/BackEndDayOffApplication.java
@@ -0,0 +1,13 @@
+package com.masterspring.backenddayoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndDayOffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndDayOffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/masterspring/backenddayoff/BackEndDayOffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masterspring.backenddayoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndDayOffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
